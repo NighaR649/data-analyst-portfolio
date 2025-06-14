e903aae813cbee2ee4880c992d78beccdf398e4a
Nigha Ravikumar <meenukashi2002@gmail.com>
2025-06-14 18:08:21 +0530

    added list sort code

---
+++ Python/listsort.py
@@ -0,0 +1,6 @@
+#wap tocount the no of students with the A grade int the following tuple.and store the valuesas list and sort them in ascending order
+grade=("C","D","A","A","B","B","A")
+print(grade.count("A"))
+grade = ["C","D","A","A","B","B","A"]
+grade.sort()
+print(grade)
